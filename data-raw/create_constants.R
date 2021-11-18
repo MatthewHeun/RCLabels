71abdf175f9390dfad262f6ea022570f68e6ef0b
MatthewHeun <matthew.heun@me.com>
2021-11-18 15:54:39 -0500

    Added prepositions

--- data-raw/create_constants.R
+++ data-raw/create_constants.R
@@ -8,6 +8,7 @@
 arrow_notation <- notation_vec(sep = " -> ")
 usethis::use_data(arrow_notation, overwrite = TRUE)
 
+
 #
 # Paren notation: a (b)
 #
@@ -18,6 +19,7 @@ paren_notation <- notation_vec(sep = "",
                                suff_start = " (",
                                suff_end = ")")
 usethis::use_data(paren_notation, overwrite = TRUE)
+
 
 #
 # Bracket notation: a [b]
@@ -30,12 +32,14 @@ bracket_notation <- notation_vec(sep = "",
                                  suff_end = "]")
 usethis::use_data(bracket_notation, overwrite = TRUE)
 
+
 #
 # From notation: a [from b]
 #
 
 from_notation <- preposition_notation(preposition = "from")
 usethis::use_data(from_notation, overwrite = TRUE)
+
 
 #
 # Of notation: a [of b]
@@ -44,6 +48,7 @@ usethis::use_data(from_notation, overwrite = TRUE)
 of_notation <- preposition_notation(preposition = "of")
 usethis::use_data(of_notation, overwrite = TRUE)
 
+
 #
 # To notation: a [to b]
 #
@@ -51,9 +56,18 @@ usethis::use_data(of_notation, overwrite = TRUE)
 to_notation <- preposition_notation(preposition = "to")
 usethis::use_data(to_notation, overwrite = TRUE)
 
+
 #
 # Bracket arrow notation: a [-> b]
 #
 
 bracket_arrow_notation <- preposition_notation(preposition = "->")
 usethis::use_data(bracket_arrow_notation, overwrite = TRUE)
+
+
+#
+# Prepositions
+#
+
+prepositions <- c("in", "into", "from", "of", "->")
+usethis::use_data(prepositions, overwrite = TRUE)
