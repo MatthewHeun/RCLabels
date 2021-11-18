3090072be4e0c7e1ab9e81c422b651c601151b85
MatthewHeun <matthew.heun@me.com>
2021-11-18 14:28:00 -0500

    Moved notation code in.

--- R/notation.R
+++ R/notation.R
@@ -31,9 +31,6 @@
 #'                      and the rejected part.
 #' * `switch_notation()` Switches from one type of notation to another based on the `from` and `to` arguments.
 #'                       Optionally, prefix and suffix can be `flip`ped.
-#' * `switch_notation_byname()` Switches matrix row and/or column names from one type of notation to another
-#'                              based on the `from` and `to` arguments.
-#'                              Optionally, prefix and suffix can be `flip`ped.
 #'
 #' If `sep` only is specified (default is " -> "),
 #' `pref_start`, `pref_end`, `suff_start`, and `suff_end` are
@@ -59,8 +56,6 @@
 #' @param from The `notation` to switch _away from_.
 #' @param to The `notation` to switch _to_.
 #' @param flip A boolean that tells whether to also flip the notation. Default is `FALSE`.
-#' @param a A matrix or list of matrices whose row and/or column notation is to be changed.
-#' @param margin `1` For rows, `2` for columns, or `c(1, 2)` for both rows and columns. Default is `c(1, 2)`.
 #' @param preposition A string used to indicate position for energy flows, typically "from" or "to" in different notations.
 #'
 #' @return For `notation_vec()`, `arrow_notation()`, and `bracket_notation()`,
@@ -70,8 +65,6 @@
 #'           a string list in notation format specified by various `notation` arguments, including
 #'           `from`, and `to`.
 #'         For `keep_pref_suff`, one of the prefix or suffix or a list of prefixes or suffixes.
-#'         For `switch_row_col_notation_byname()`, matrices with row and column names with switched notation,
-#'           per arguments.
 #'
 #' @examples
 #' notation_vec()
@@ -83,18 +76,8 @@
 #' switch_notation("a -> b", from = arrow_notation(), to = bracket_notation())
 #' switch_notation("a -> b", from = arrow_notation(), to = bracket_notation(),
 #'                 flip = TRUE)
-#' m <- matrix(c(1, 2,
-#'               3, 4), nrow = 2, ncol = 2, byrow = TRUE,
-#'             dimnames = list(c("b [a]", "d [c]"), c("f [e]", "h [g]"))) %>%
-#'   setrowtype("Products [Industries]") %>% setcoltype("Industries [Products]")
-#' m
-#' switch_notation_byname(m, from = bracket_notation(), to = arrow_notation(),
-#'                        flip = TRUE)
-#' # Also works for lists.
-#' # Note that margin must be specified as a list here.
-#' switch_notation_byname(list(m, m), margin = list(c(1, 2)),
-#'                        from = bracket_notation(),
-#'                        to = arrow_notation(), flip = TRUE)
+#' # Also works for vectors
+#' switch_notation(c("a -> b", "c -> d"), from = arrow_notation(), to = bracket_notation())
 #' @name row-col-notation
 NULL
 
@@ -266,7 +249,7 @@ keep_pref_suff <- function(x, keep = c("pref", "suff"), notation) {
 
   pref_suff <- split_pref_suff(x, notation)
   if (length(x) > 1) {
-    out <- lapply(pref_suff, choose_pref_or_suff, keep) %>%
+    out <- lapply(pref_suff, choose_pref_or_suff, keep) |>
       unlist()
   } else {
     out <- choose_pref_or_suff(pref_suff, keep)
@@ -297,50 +280,3 @@ switch_notation <- function(x, from, to, flip = FALSE) {
   switch_func(x)
 }
 
-
-#' #' @export
-#' #' @rdname row-col-notation
-#' switch_notation_byname <- function(a, margin = c(1, 2), from, to, flip = FALSE) {
-#'   margin <- prep_vector_arg(a, margin)
-#'   from <- prep_vector_arg(a, from)
-#'   to <- prep_vector_arg(a, to)
-#'   flip <- prep_vector_arg(a, flip)
-#'   switch_func <- function(a_mat, margin, from, to, flip) {
-#'     # When we get here, we should have a single matrix a_mat.
-#'     assertthat::assert_that(all(margin %in% c(1, 2)), msg = paste0("In switch_notation_byname, margin must be 1, 2, or both. ",
-#'                                                                    "Found margin = ", paste(margin, collapse = ", ")))
-#'
-#'     out <- a_mat
-#'     if (2 %in% margin) {
-#'       # Transpose the matrices
-#'       transposed <- matsbyname::transpose_byname(out)
-#'       # re-call with margin = 1 to change from arrow to paren notation on the rows (which are really columns)
-#'       switched <- switch_notation_byname(transposed, margin = 1, from = from, to = to, flip = flip)
-#'       # Transpose
-#'       out <- transpose_byname(switched)
-#'     }
-#'     if (1 %in% margin) {
-#'       # Get the row names
-#'       old_rownames <- getrownames_byname(out)
-#'       # call func on old row names to create new row names
-#'       new_rownames <- switch_notation(old_rownames, from = from, to = to, flip = flip)
-#'       # Set row names to the new row names
-#'       out <- setrownames_byname(out, new_rownames)
-#'       # Perform the same transformation on the row type, but only if we had a rowtype in a_mat
-#'       if (!is.null(rowtype(out))) {
-#'         old_rowtype <- rowtype(out)
-#'         new_rowtype <- switch_notation(old_rowtype,
-#'                                        from = from, to = to, flip = flip)
-#'         out <- out %>% setrowtype(new_rowtype)
-#'       }
-#'     }
-#'     # Return the result
-#'     return(out)
-#'   }
-#'
-#'   unaryapply_byname(switch_func, a,
-#'                     .FUNdots = list(margin = margin, from = from, to = to, flip = flip),
-#'                     # We control row and column types in this function, so
-#'                     # prevent unaryapply_byname from setting them.
-#'                     rowcoltypes = "none")
-#' }
--- tests/testthat/test-notation.R
+++ tests/testthat/test-notation.R
@@ -108,7 +108,7 @@ test_that("split_pref_suff() works properly", {
 
 test_that("split_pref_suff() works in a data frame", {
   df <- data.frame(donottouch = c(1, 2), orig = c("a -> b", "c -> d"))
-  splitted <- df %>%
+  splitted <- df |>
     dplyr::mutate(
       split = split_pref_suff(orig, notation = arrow_notation())
     )
@@ -120,24 +120,24 @@ test_that("join_pref_suff() works properly", {
   ps <- list(pref = "a", suff = "b")
   expect_equal(paste_pref_suff(ps, notation = arrow_notation()), "a -> b")
   # Make sure that they are the inverse of each other
-  expect_equal(paste_pref_suff(ps, notation = arrow_notation()) %>%
+  expect_equal(paste_pref_suff(ps, notation = arrow_notation()) |>
                  split_pref_suff(notation = arrow_notation()), ps)
   # Try with paren notation list
-  expect_equal(paste_pref_suff(ps, notation = bracket_notation()) %>%
+  expect_equal(paste_pref_suff(ps, notation = bracket_notation()) |>
                  split_pref_suff(notation = bracket_notation()), ps)
   # Try with a wacky notation list
   amp_nl <- notation_vec(sep = "&&&&&&&&")
-  expect_equal(paste_pref_suff(ps, notation = amp_nl) %>%
+  expect_equal(paste_pref_suff(ps, notation = amp_nl) |>
                  split_pref_suff(notation = amp_nl), ps)
   paren_nl <- notation_vec(pref_start = "(", pref_end = ")",
                            suff_start = "(", suff_end = ")")
-  expect_equal(paste_pref_suff(ps, notation = paren_nl) %>%
+  expect_equal(paste_pref_suff(ps, notation = paren_nl) |>
                  split_pref_suff(notation = paren_nl), ps)
   # Try to join lists
   expect_equal(paste_pref_suff(list(list(pref = "a", suff = "b"), list(pref = "c", suff = "d"))), list("a -> b", "c -> d"))
   # Try to split then join lists
   joined <- list("a -> b", "c -> d")
-  expect_equal(split_pref_suff(joined, notation = arrow_notation()) %>%
+  expect_equal(split_pref_suff(joined, notation = arrow_notation()) |>
                  paste_pref_suff(notation = arrow_notation()),
                joined)
 
@@ -192,7 +192,7 @@ test_that("keep_pref_suff() works as expected", {
 
   # Test in a data frame using mutate.
   df <- data.frame(v1 = c("a -> b", "c -> d"), v2 = c("e [f]", "g [h]"))
-  res <- df %>%
+  res <- df |>
     dplyr::mutate(
       # Keep the prefixes from the arrow notation column (v1)
       pref = keep_pref_suff(v1, keep = "pref", notation = arrow_notation()),
@@ -248,85 +248,9 @@ test_that("switch_notation() works as expected", {
 
 test_that("switch_notation() works in a data frame", {
   df <- data.frame(orig = c("a -> b", "c -> d"))
-  switched <- df %>%
+  switched <- df |>
     dplyr::mutate(
       new = switch_notation(orig, from = arrow_notation(), to = bracket_notation())
     )
   expect_equal(switched$new, list("a [b]", "c [d]"))
 })
-
-
-# test_that("switch_notation_byname() works as expected", {
-#   # Switch row names
-#   m <- matrix(c(1, 2,
-#                 3, 4), nrow = 2, ncol = 2, byrow = TRUE, dimnames = list(c("a [b]", "c [d]"), c("e", "f")))
-#   e <- m
-#   rownames(e) <- c("a -> b", "c -> d")
-#   expect_equal(switch_notation_byname(m, margin = 1, from = bracket_notation(), to = arrow_notation()), e)
-#
-#   # Switch column names
-#   m2 <- matrix(c(1, 2,
-#                  3, 4), nrow = 2, ncol = 2, byrow = TRUE, dimnames = list(c("a", "b"), c("d [c]", "f [e]"))) %>%
-#     setrowtype("rows") %>% setcoltype("Industries [Products]")
-#   e2 <- m2
-#   colnames(e2) <- c("d -> c", "f -> e")
-#   e2 <- e2 %>% setcoltype("Industries -> Products")
-#   expect_equal(switch_notation_byname(m2, margin = 2, from = bracket_notation(), to = arrow_notation()), e2)
-#
-#   # Also flip the prefix and suffix. Verify that changes are made in the coltype, too.
-#   m3 <- matrix(c(1, 2,
-#                  3, 4), nrow = 2, ncol = 2, byrow = TRUE, dimnames = list(c("a", "b"), c("d [c]", "f [e]"))) %>%
-#     setrowtype("rows") %>% setcoltype("Industries [Products]")
-#   e3 <- m3
-#   colnames(e3) <- c("c -> d", "e -> f")
-#   e3 <- e3 %>% setcoltype("Products -> Industries")
-#   expect_equal(switch_notation_byname(m3, from = bracket_notation(), to = arrow_notation(), flip = TRUE), e3)
-#
-#   # Switch both row and column names
-#   m4 <- matrix(c(1, 2,
-#                  3, 4), nrow = 2, ncol = 2, byrow = TRUE, dimnames = list(c("b [a]", "d [c]"), c("f [e]", "h [g]"))) %>%
-#     setrowtype("Products [Industries]") %>% setcoltype("Industries [Products]")
-#   e4 <- m4
-#   rownames(e4) <- c("a -> b", "c -> d")
-#   colnames(e4) <- c("e -> f", "g -> h")
-#   e4 <- e4 %>% setrowtype("Industries -> Products") %>% setcoltype("Products -> Industries")
-#   expect_equal(switch_notation_byname(m4,
-#                                       from = bracket_notation(),
-#                                       to = arrow_notation(),
-#                                       flip = TRUE),
-#                e4)
-#
-#   # Try with a list
-#   expect_equal(switch_notation_byname(list(m4, m4), margin = list(c(1, 2)),
-#                                       from = list(bracket_notation()),
-#                                       to = list(arrow_notation()),
-#                                       flip = TRUE),
-#                list(e4, e4))
-#
-#   # Try degenerate case of no row and column types.
-#   m5 <- matrix(c(1, 2,
-#                  3, 4), nrow = 2, ncol = 2, byrow = TRUE, dimnames = list(c("a -> b", "c -> d"), c("c1", "c2")))
-#
-#   expected <- m5
-#   rownames(expected) <- c("b [a]", "d [c]")
-#   actual <- switch_notation_byname(m5, from = arrow_notation(), to = bracket_notation(), flip = TRUE)
-#   expect_equal(rownames(actual), rownames(expected))
-#   expect_null(rowtype(actual))
-#   expect_null(coltype(actual))
-#   expect_equal(actual, expected)
-# })
-#
-#
-# test_that("switch_notation_byname() works well when flip is a list", {
-#   m <- matrix(c(1, 2,
-#                 3, 4), nrow = 2, ncol = 2, byrow = TRUE, dimnames = list(c("a", "b"), c("d [c]", "f [e]"))) %>%
-#     setrowtype("rows") %>% setcoltype("Industries [Products]")
-#   e <- m
-#   colnames(e) <- c("c -> d", "e -> f")
-#   e <- e %>% setcoltype("Products -> Industries")
-#   expect_equal(switch_notation_byname(m, from = bracket_notation(), to = arrow_notation(), flip = list(TRUE)), e)
-# })
-#
-
-
-
