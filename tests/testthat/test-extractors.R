d46d5ebdcd85ffaeba5c0b4854fbbd8d747192eb
MatthewHeun <matthew.heun@me.com>
2021-11-21 16:57:54 -0500

    Debugging tests

--- tests/testthat/test-extractors.R
+++ tests/testthat/test-extractors.R
@@ -10,6 +10,8 @@ test_that("get_noun() works as expected", {
     dplyr::mutate(
       nouns = get_nouns(labels)
     )
+  expect_equal(with_nouns$nouns[[1]], c("a", "c"))
+  expect_equal(with_nouns$nouns[[2]], c("e", "g"))
 })
 
 
@@ -20,11 +22,21 @@ test_that("get_pps() works as expected", {
   expect_equal(get_pps(list("a [of b in c]", "d [of e into f]"), bracket_notation),
                list(list("of b", "in c"), list("of e", "into f")))
 
-
-
   expect_equal(get_pps("a [in b]"), list(list("in b")))
   expect_equal(get_pps(c("a [in b]")), list(list("in b")))
   expect_equal(get_pps(list("a [in b]")), list(list("in b")))
+
+  # Now try in a data frame
+
+  df <- data.frame(labels = I(list(list("a [in b]", "c [of d]"),
+                                   list("e [of f in g]", "h [-> i in j]"))))
+  with_nouns <- df |>
+    dplyr::mutate(
+      nouns = get_nouns(labels),
+      pps = get_pps(labels)
+    )
+  expect_equal(with_nouns$nouns[[1]], c("a", "c"))
+  expect_equal(with_nouns$nouns[[2]], c("e", "g"))
 
 
 })
