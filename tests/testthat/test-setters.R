04a89de918b9dbb826d38aab7126dfb4b4811dc6
MatthewHeun <MatthewHeun@users.noreply.github.com>
2021-11-27 10:48:33 -0500

    Better test.

--- tests/testthat/test-setters.R
+++ tests/testthat/test-setters.R
@@ -3,6 +3,6 @@ test_that("set_nouns() works as expected", {
   expect_equal(set_nouns(label, "d"), "d [of b in c]")
 
   labels <- c("a [of b in c]", "d [of e in USA]")
-  expect_equal(set_nouns(labels, c("f", "g")),
-               c("f [of b in c]", "g [of e in USA]"))
+  expect_equal(set_nouns(labels, c("a_plus", "g")),
+               c("a_plus [of b in c]", "g [of e in USA]"))
 })
