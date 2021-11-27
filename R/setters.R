fa82bf9654a1370592b4ecbab6cd1e278d27e93f
MatthewHeun <MatthewHeun@users.noreply.github.com>
2021-11-27 10:49:47 -0500

    Example for set_nouns

--- R/setters.R
+++ R/setters.R
@@ -14,6 +14,8 @@
 #' @export
 #'
 #' @examples
+#' labels <- c("a [of b in c]", "d [of e in USA]")
+#' set_nouns(labels, c("a_plus", "g"))
 set_nouns <- function(labels, new_nouns, notation = RCLabels::bracket_notation) {
   splitted <- split_labels(labels)
   mapply(splitted, new_nouns, SIMPLIFY = FALSE, FUN = function(this_split_label, this_new_noun) {
